7e45a9c1a860919aaba836cb3707b539700a4f6d
Trong <tronga7z6@gmail.com>
2023-06-16 17:10:46 +0700

    edit admin after js

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const port = 3000;
 
 app.use(morgan("combined"));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "Admin.html"));
+  res.sendFile(path.join(__dirname, "public/views/Admin.html"));
 });
 
 app.listen(port, () => {
